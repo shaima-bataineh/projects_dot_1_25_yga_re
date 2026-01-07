c8b54de4f174a23169f6c104ebaf8a819149f90f
shaimaa.SB <shaimabti0@gmail.com>
2026-01-07 17:37:01 +0300

    Add files via upload

---
+++ SalesFlow/jscript/scr.js
@@ -0,0 +1,55 @@
+
+//DOM elements
+const businessnameInput = document.getElementById("businessname");
+const demoButton = document.getElementById("demoBtn");
+const demoMessage = document.getElementById("demoMessage");
+
+// default message
+const defaultMessage = "Please enter your Business Name to continue.";
+demoMessage.textContent = defaultMessage;
+demoButton.disabled = true;
+
+
+// enable button only when input is not empty
+businessnameInput.addEventListener("input", function () {
+  if (businessnameInput.value === "") {
+    demoButton.disabled = true;
+    demoMessage.textContent = defaultMessage;
+    demoMessage.classList.remove('active-message');
+  } else {
+    demoButton.disabled = false;
+  }
+});
+
+// button click changes text
+demoButton.addEventListener("click", function () {
+    demoMessage.textContent = "Welcome, " + businessnameInput.value + "! let's get started with your sales flow demo.";
+    demoMessage.classList.add("active-message");
+});
+
+// DOM element
+const titleElement = document.getElementById("typingTitle");
+
+// get full text
+const fullText = titleElement.getAttribute("data-text");
+
+// split text into words
+const words = fullText.split(" ");
+
+let wordIndex = 0;
+const speed = 300; // speed between words 
+
+function typeWords() {
+  if (wordIndex < words.length) {
+    titleElement.textContent += words[wordIndex] + " ";
+    wordIndex++;
+    setTimeout(typeWords, speed);
+  }
+   else {
+    // hide cursor after typing is done
+    titleElement.classList.add("cursor-hidden");
+  }
+}
+
+// start typing after page loads
+document.addEventListener("DOMContentLoaded", typeWords);
