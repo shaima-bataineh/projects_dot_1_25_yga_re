53d2f11aa1acc198bd9304587f627eac3bab736e
shaimaa.SB <shaimabti0@gmail.com>
2026-01-23 14:27:43 +0300

    remove edit from my main project to avoid the errors

--- app-salesflow/src/App.jsx
+++ app-salesflow/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import "./css/Custom1.css";
 
 function App() {
